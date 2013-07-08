33bebe629afadc3c4d64ee4eb1a76b8c922e591f
Vasileios Triglianos <triglian@gmail.com>
2013-07-09 00:34:07 +0300

    refactored

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-var express = require('express');
-var main = require('./routes/main')
-var app = express();
+var express = require('express')
+, main = require('./routes/main')
+, app = express()
+, path = require('path');
 
 mongoose = require('mongoose');
 db = mongoose.createConnection('localhost', 'challenge');
@@ -9,12 +10,10 @@ schemas = require('./models/models.js');
 
 app.configure(function() {
     app.set('port', process.env.PORT || 3000);
-    app.use(express.bodyParser());
     app.set('views', __dirname + '/views');
     app.set('view engine', 'ejs');
-    app.use("/styles", express.static(__dirname + '/styles'));
-    app.use("/codemirror", express.static(__dirname + '/codemirror'));
-    app.use("/ace", express.static(__dirname + '/ace'));
+    app.use(express.bodyParser());
+    app.use(express.static(__dirname + '/public'));
     
 });
 
@@ -24,4 +23,6 @@ app.post('/', main.submit);
 
 
 
-app.listen(3000);
+app.listen(3000, function(){
+  console.log("Express server listening on port 3000");
+});
