d88ba58219eee1ebe55b69fe2cfd2443cc5a72de
Vasileios Triglianos <triglian@gmail.com>
2013-07-09 20:50:33 +0300

    changed event to changeCursor

--- public/js/highlight.js
+++ public/js/highlight.js
@@ -1,15 +1,15 @@
- function formIntercept() {
-      var editor = $('#myTextArea').data('CodeMirrorInstance');
-      var markers = editor.getAllMarks();
-      var toJson = new Array();
-      for (var i = 0; i < markers.length; i++) {
-      toJson.push(markers[i].find());
-      }
-      var finalSend = new Array();
-      finalSend.push(editor.getValue());
-      finalSend.push(toJson);
-      document.getElementById("submitform").elements["hidden"].value = JSON.stringify(finalSend);
-      }
+ // function formIntercept() {
+ //      var editor = $('#myTextArea').data('CodeMirrorInstance');
+ //      var markers = editor.getAllMarks();
+ //      var toJson = new Array();
+ //      for (var i = 0; i < markers.length; i++) {
+ //      toJson.push(markers[i].find());
+ //      }
+ //      var finalSend = new Array();
+ //      finalSend.push(editor.getValue());
+ //      finalSend.push(toJson);
+ //      document.getElementById("submitform").elements["hidden"].value = JSON.stringify(finalSend);
+ //      }
  window.onload = function () {
       
       /// CLEAN ME UP - WATCH FOR CONFLICTS
@@ -34,7 +34,7 @@
       , aceEditSession =  aceeditor.getSession()
 
       //handle selection events
-      aceEditSession.selection.on('changeSelection', function(e) {
+      aceEditSession.selection.on('changeCursor', function(e) {
           console.log(aceEditSession.selection.isEmpty());
           
           if (!aceEditSession.selection.isEmpty()) {
@@ -53,62 +53,62 @@
       
       
       
-      document.getElementById("clear").onclick = function() {
-      var markers = editor.getAllMarks();
-      for (var i = 0; i < markers.length; i++) {
-      markers[i].clear();
-      }
+      // document.getElementById("clear").onclick = function() {
+      // var markers = editor.getAllMarks();
+      // for (var i = 0; i < markers.length; i++) {
+      // markers[i].clear();
+      // }
  
-      }
+      // }
       
-      $('.color').click(function() {
-      colorclass = $(this).attr("id");
-      console.log(colorclass);
-      });
+      // $('.color').click(function() {
+      // colorclass = $(this).attr("id");
+      // console.log(colorclass);
+      // });
 
       
-      var oldstart = ({line: 0, ch: 0});
-      var oldend = ({line: 0, ch: 0});
+      // var oldstart = ({line: 0, ch: 0});
+      // var oldend = ({line: 0, ch: 0});
       
-      var colorclass = "red";
+      // var colorclass = "red";
       
-      editor.on("cursorActivity", function() {
-      var markerscursor = editor.findMarksAt(editor.getCursor("start"));
-      var existsmarker = false;
-      if (markerscursor.length >0) {
-      if (editor.somethingSelected()) {
+      // editor.on("cursorActivity", function() {
+      // var markerscursor = editor.findMarksAt(editor.getCursor("start"));
+      // var existsmarker = false;
+      // if (markerscursor.length >0) {
+      // if (editor.somethingSelected()) {
       
-      for (var i = 0; i< markerscursor.length; i++) {
-            if (markerscursor[i].className == colorclass) {
-            existsmarker = true;
-            var start = markerscursor[i].find().from;
-            var end = markerscursor[i].find().to;
-            markerscursor[i].clear();
-            editor.markText(start,editor.getCursor("start"), {className: colorclass});
-            editor.markText(editor.getCursor("end"), end ,{className: colorclass});
-            }
-      }
-      }
-      }
+      // for (var i = 0; i< markerscursor.length; i++) {
+      //       if (markerscursor[i].className == colorclass) {
+      //       existsmarker = true;
+      //       var start = markerscursor[i].find().from;
+      //       var end = markerscursor[i].find().to;
+      //       markerscursor[i].clear();
+      //       editor.markText(start,editor.getCursor("start"), {className: colorclass});
+      //       editor.markText(editor.getCursor("end"), end ,{className: colorclass});
+      //       }
+      // }
+      // }
+      // }
       
-      if (!existsmarker) {
-      if (oldstart.line == editor.getCursor("start").line && oldstart.ch == editor.getCursor("start").ch && editor.somethingSelected()) {
-      oldend = editor.getCursor("end");
-      } else {
+      // if (!existsmarker) {
+      // if (oldstart.line == editor.getCursor("start").line && oldstart.ch == editor.getCursor("start").ch && editor.somethingSelected()) {
+      // oldend = editor.getCursor("end");
+      // } else {
       
-      if (oldend.line == editor.getCursor("end").line && oldend.ch == editor.getCursor("end").ch && editor.somethingSelected()) {
-            oldstart = editor.getCursor("start");
-      } else {
+      // if (oldend.line == editor.getCursor("end").line && oldend.ch == editor.getCursor("end").ch && editor.somethingSelected()) {
+      //       oldstart = editor.getCursor("start");
+      // } else {
             
-            editor.markText(oldstart, oldend, {className: colorclass});
-            oldstart = editor.getCursor("start");
-            oldend = editor.getCursor("end"); 
-      }
-      }  
-      }
-      console.log(editor.getAllMarks());
+      //       editor.markText(oldstart, oldend, {className: colorclass});
+      //       oldstart = editor.getCursor("start");
+      //       oldend = editor.getCursor("end"); 
+      // }
+      // }  
+      // }
+      // console.log(editor.getAllMarks());
       
-      });
+      // });
 
  }
       
